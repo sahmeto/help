3941b3e9992ee54cd879f54c5c9234f11352501f
Ehsan Akbarzadeh <ehsaneona@gmail.com>
2023-03-27 13:41:55 +0330

    :tada: feat: start sahmeto help center

---
+++ docusaurus.config.js
@@ -0,0 +1,84 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+    title: 'مستندات سهمتو',
+    favicon: 'img/favicon.ico',
+
+    // Set the production url of your site here
+    url: 'https://sahmeto.com/',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/',
+
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'sahmeto', // Usually your GitHub org/user name.
+    projectName: 'sahmeto-help', // Usually your repo name.
+
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+
+    // Even if you don't use internalization, you can use this field to set useful
+    // metadata like html lang. For example, if your site is Chinese, you may want
+    // to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'fa',
+        locales: ['fa'],
+        path: 'i18n',
+        localeConfigs: {
+            fa: {
+                label: 'فارسی',
+                direction: 'rtl',
+                htmlLang: 'fa-IR',
+                calendar: 'persian',
+                path: 'fa',
+            },
+        },
+    },
+
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    routeBasePath: '/',
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            }),
+        ],
+    ],
+
+    themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            navbar: {
+                title: 'مستندات سهمتو',
+                logo: {
+                    alt: 'سهمتو',
+                    src: 'img/logo.svg',
+                },
+                items: [
+                    {
+                        href: 'https://sahmeto.com/',
+                        label: 'وبسایت',
+                        position: 'right',
+                    },
+                ],
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+            },
+        }),
+};
+
+module.exports = config;
