444504ca47b47ed70a0875710513b39472151f47
Ehsan Akbarzadeh <ehsaneona@gmail.com>
2023-04-20 10:56:38 +0330

    :sparkles: feat: new channel page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,7 +78,18 @@ const config = {
                 theme: lightCodeTheme,
                 darkTheme: darkCodeTheme,
             },
+            zoom: {
+                selector: '.markdown :not(em) > img',
+                config: {
+                    // options you can specify via https://github.com/francoischalifour/medium-zoom#usage
+                    background: {
+                        light: 'rgb(255, 255, 255)',
+                        dark: 'rgb(50, 50, 50)'
+                    }
+                }
+            }
         }),
+    plugins: [require.resolve("docusaurus-plugin-image-zoom")],
 };
 
 module.exports = config;
